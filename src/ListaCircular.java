45c38b5d5a30b8e59c1bbcf6e372f904b723e95c
jguih15 <jguih15@gmail.com>
2025-09-12 17:32:55 -0300

    primeira commit

---
+++ src/ListaCircular.java
@@ -0,0 +1,51 @@
+public class ListaCircular {
+    private Node atual;
+    private int tamanho;
+    private Node cabeca;
+
+    public ListaCircular(Processos processo) {
+        this.atual=null;
+        this.tamanho=0;
+    }
+    public void addProcessoLC(Processos processo) {
+        Node novoNo = new Node();
+        if (atual == null) {
+            atual = novoNo;
+            novoNo.next = atual;
+        }
+        else {
+            Node temp = cabeca;
+            while (temp.next != cabeca) {
+                temp = temp.next;
+            }
+            temp.next = novoNo;
+            novoNo.next = cabeca;
+        }
+        tamanho++;
+    }
+    public void removerProcessoLC(Processos processo) {
+        if  (atual == null)
+            return;
+
+        else if (atual.next == cabeca) {
+            processo = atual.processo;
+            return;
+        }else{
+            Node temp = cabeca;
+            while (temp.next != atual) {
+                temp = temp.next;
+            }
+            temp.next = cabeca.next;
+            cabeca = temp.next;
+        }
+        tamanho--;
+    }
+    public void removerUltimoProcessoLC(Processos processo) {
+        if  (atual == null)
+           return;
+        else  if (atual.next == cabeca) {}
+
+    }
+
+}
+
